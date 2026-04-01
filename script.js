ead79ed04d665b06b6cb6e49ab9b36e8c555c270
ShurjeelCS <shurjeel_m@hotmail.com>
2026-04-01 15:06:46 +0100

    temperature is shown to user on page

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+const container = document.querySelector('.container');
+
 async function getWeather(location) {
     try {
         const weather = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' + location + '?unitGroup=uk&key=8NGT5AXWUHYCEYFBXN5CDWFY2')
         const data = await weather.json();
         console.log(data.currentConditions.temp);
+        showWeather(data.currentConditions.temp);
     } catch (error) {
         console.log(error);
     }
@@ -14,3 +17,10 @@ button.addEventListener("click", () => {
     const location = document.querySelector("#location").value;
     getWeather(location)
 });
+
+function showWeather(temp) {
+    const weather = document.createElement("p");
+    weather.textContent = temp;
+    container.innerHTML = "";
+    container.appendChild(weather);
+}
