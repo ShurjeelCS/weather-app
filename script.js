f7868f2dec4144dcff50bbaa5db4d2ff23504e72
ShurjeelCS <shurjeel_m@hotmail.com>
2026-04-01 13:57:43 +0100

    added search bar for locations

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-console.log("hello worlds");
-
-async function getWeather() {
+async function getWeather(location) {
     try {
-        const weather = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=8NGT5AXWUHYCEYFBXN5CDWFY2')
+        const weather = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' + location + '?key=8NGT5AXWUHYCEYFBXN5CDWFY2')
         const data = await weather.json();
         console.log(data);
     } catch (error) {
@@ -11,4 +9,8 @@ async function getWeather() {
     
 }   
 
-getWeather();
+const button = document.querySelector('.submit');
+button.addEventListener("click", () => {
+    const location = document.querySelector("#location").value;
+    getWeather(location)
+});
